a8d2426d108a217ffdc689bdcca5b7faa87043af
swagner210 <44910380+swagner210@users.noreply.github.com>
2019-02-13 14:53:34 -0800

    Create prog1_2.h

---
+++ prog1_2.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct stack{
+    int* data;
+    int size;
+    int capacity;
+}STACK;
+
+STACK* MakeStack(int initialCapacity);
+void Push(STACK *stackPtr,int data);
+int Pop(STACK *stackPtr);
+void Grow(STACK *stackPtr);
